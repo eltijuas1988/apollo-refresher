f7f1859923c5906667f32ccc9820fd6f7a753484
Daniel Rojas <crimsondex@yahoo.com>
2019-03-05 12:28:36 -0800

    Created resolvers for user, launch and launches.

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,6 +1,7 @@
 const {ApolloServer} = require('apollo-server')
 const {createStore} = require('./utils')
 const typeDefs = require('./schema')
+const resolvers = require('./resolvers')
 const LaunchAPI = require('./datasources/launch')
 const UserAPI = require('./datasources/user')
 
@@ -8,6 +9,7 @@ const store = createStore()
 
 const server = new ApolloServer({
   typeDefs,
+  resolvers,
   dataSources: () => ({
     launchAPI: new LaunchAPI(),
     userAPI: new UserAPI({store}),
