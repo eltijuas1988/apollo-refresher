fd159bb4e34484b941a8e85c5f7c967e2d232dca
Daniel Rojas <crimsondex@yahoo.com>
2019-03-10 22:59:08 -0700

    Added details page for individual launches.

---
+++ start/client/src/containers/action-button.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ActionButton = () => {
+  return <div>Action Button Will Go here</div>
+}
+
+export default ActionButton
--- start/client/src/pages/launch.js
+++ start/client/src/pages/launch.js
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, {Fragment} from 'react'
+import {Query} from 'react-apollo'
+import gql from 'graphql-tag'
+import {Loading, Header, LaunchDetail} from '../components'
+import ActionButton from '../containers/action-button'
 
-export default function Launch() {
+export const GET_LAUNCH_DETAILS = gql`
+  query LaunchDetails($launchId: ID!) {
+    launch(id: $launchId) {
+      id
+      site
+      isBooked
+      rocket {
+        id
+        name
+        type
+      }
+      mission {
+        name
+        missionPatch
+      }
+    }
+  }
+`
+
+export default function Launch({launchId}) {
   return (
-    <div>This is the launch</div>
+    <Query query={GET_LAUNCH_DETAILS} variables={{launchId}}>
+      {({data, loading, error}) => {
+        if (loading) return <Loading/>
+        if (error) return <p>ERROR: {error.message}</p>
+
+        return (
+          <Fragment>
+            <Header image={data.launch.mission.missionPatch}>
+              {data.launch.mission.name}
+            </Header>
+            <LaunchDetail {...data.launch}/>
+            <ActionButton {...data.launch}/>
+          </Fragment>
+        )
+      }}
+    </Query>
   )
 }
