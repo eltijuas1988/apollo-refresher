a24200308e3f5c8b073dc8da1dd7111f3d19a578
Daniel Rojas <crimsondex@yahoo.com>
2019-03-12 11:08:20 -0700

    Created profile component with query that fetches profile data.

--- start/client/src/pages/profile.js
+++ start/client/src/pages/profile.js
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, {Fragment} from 'react'
+import {Query} from 'react-apollo'
+import gql from 'graphql-tag'
+import {Loading, Header, LaunchTile} from '../components'
+import {LAUNCH_TILE_DATA} from './launches'
+
+const GET_MY_TRIPS = gql`
+  query GetMyTrips {
+    me {
+      id
+      email
+      trips {
+        ...LaunchTile
+      }
+    }
+  }
+  ${LAUNCH_TILE_DATA}
+`
 
 export default function Profile() {
   return (
-    <div>Profile will go here</div>
+    <Query query={GET_MY_TRIPS} fetchPolicy="network-only">
+      {({data, loading, error}) => {
+        if (loading) return <Loading/>
+        if (error) return <p>ERROR: {error.message}</p>
+
+        return (
+          <Fragment>
+            <Header>My Trips</Header>
+            {data.me && data.me.trips.length? (
+              data.me.trips.map(launch => (
+                <LaunchTile key={launch.id} launch={launch}/>
+              ))
+            ) : (
+              <p>You Haven't booked any trips</p>
+            )}
+          </Fragment>
+        )
+      }}
+    </Query>
   )
 }
