98838e3ca5856cf0924384072d3e13e4d1c16582
Daniel Rojas <crimsondex@yahoo.com>
2019-03-13 11:58:06 -0700

    Updated client/index to only show Pages after user logs in.

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import gql from 'graphql-tag'
 import {ApolloClient} from 'apollo-client'
 import {InMemoryCache} from 'apollo-cache-inmemory'
 import {HttpLink} from 'apollo-link-http'
-import {ApolloProvider} from 'react-apollo'
+import {ApolloProvider, Query} from 'react-apollo'
+import injectStyles from './styles';
 import Pages from './pages'
+import Login from './pages/login'
 
 const cache = new InMemoryCache()
 const link = new HttpLink({
@@ -25,8 +28,17 @@ cache.writeData({
   }
 })
 
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`
+
+injectStyles()
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Pages/>
+    <Query query={IS_LOGGED_IN}>
+      {({data}) => (data.isLoggedIn ? <Pages/> : <Login/>)}
+    </Query>
   </ApolloProvider>, document.getElementById('root')
 );
